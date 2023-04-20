908908a97d8cf009e70c985a7595f78bb2336407
Stas Zarudensky <szarudensky@gmail.com>
2023-04-21 01:00:09 +0300

    Created the initial structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,47 +2,87 @@ import styles from './Header.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Select from '../Select/Select'
+import { useState, useRef, useEffect } from 'react'
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  const selectRef = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (
+      selectRef.current &&
+      !selectRef.current.contains(event.target) &&
+      !event.target.classList.contains(styles.menu__btn)
+    ) {
+      setIsOpen(false)
+    }
+  }
+
+  const handleSelectOption = () => {
+    setIsOpen(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [handleClickOutside])
+
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/">
-          <Image
-            className={styles.logo}
-            src="./logo.svg"
-            alt="Cleanlab"
-            width={122}
-            height={24}
-            priority
-          />
-        </Link>
+      <Link href="/">
+        <Image
+          className={styles.logo}
+          src="./logo.svg"
+          alt="Cleanlab"
+          width={122}
+          height={24}
+          priority
+        />
+      </Link>
+      <div className={styles.menu__btn} onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`${styles.menu__container} ${isOpen ? styles.open : ''}`} ref={selectRef}>
         <nav className={styles.navigation}>
-          <Link className={styles.link} href="/about">About</Link>
+          <Link className={styles.link} href="/about" onClick={() => setIsOpen(false)}>About</Link>
           <Select
             label="Products"
             options={[
-              { value: 'Product 1', href: '/Product_1' },
-              { value: 'Product 2', href: '/Product_2' },
-              { value: 'Product 3', href: '/Product_3' }
+              { value: 'Product 1', href: '/#' },
+              { value: 'Product 2', href: '/#' },
+              { value: 'Product 3', href: '/#' }
             ]}
+            onSelectOption={handleSelectOption}
           />
           <Select
             label="Solutions"
             options={[
-              { value: 'Solution 1', href: '/Solution_1' },
-              { value: 'Solution 2', href: '/Solution_2' }
+              { value: 'Solution 1', href: '/#' },
+              { value: 'Solution 2', href: '/#' }
             ]}
+            onSelectOption={handleSelectOption}
           />
           <Select
             label="Learn"
             options={[
-              { value: 'Learn 1', href: '/Learn_1' },
-              { value: 'Learn 2', href: '/Learn_2' },
-              { value: 'Learn 3', href: '/Learn_3' }
+              { value: 'Learn 1', href: '/#' },
+              { value: 'Learn 2', href: '/#' },
+              { value: 'Learn 3', href: '/#' },
+              { value: 'Learn 4', href: '/#' }
             ]}
+            onSelectOption={handleSelectOption}
           />
-          <Link className={styles.link} href="/community">Join our community</Link>
+          <Link 
+            className={styles.link} href="https://communityinviter.com/apps/cleanlab-community/cleanlab-community" 
+            target="_blank"
+            onClick={() => setIsOpen(false)}
+          >
+            Join our community
+          </Link>
         </nav>
         <button className={styles.light__btn} type="button">Get in touch</button>
         <button className={styles.hard__btn} type="button">Try for free</button>
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,14 +1,47 @@
 import styles from './Select.module.css'
 import Link from 'next/link'
+import { useState, useRef, useEffect } from 'react'
 
-export default function Select({ label, options }) {
+export default function Select({ label, options, onSelectOption }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const selectRef = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (selectRef.current && !selectRef.current.contains(event.target)) {
+      setIsOpen(false)
+    }
+  }
+
+  const handleSelectOption = () => {
+    setIsOpen(false)
+    onSelectOption()
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [handleClickOutside])
+
   return (
-    <div className={styles.select}>
-      <div className={styles.select__label}>{label}</div>
-      <div id="products" className={styles.select__options}>
+    <div ref={selectRef} className={styles.select}>
+      <div 
+        className={styles.select__label} 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {label}
+      </div>
+      <div className={`${styles.select__options} ${isOpen && styles.open}`}>
         {options.map((option) => (
           <div key={option.value} className={styles.select__option}>
-            <Link className={styles.link} href={option.href}>{option.value}</Link>
+            <Link 
+              className={styles.link} 
+              href={option.href}
+              onClick={() => handleSelectOption(option)}
+            >
+              {option.value}
+            </Link>
           </div>
         ))}
       </div>
